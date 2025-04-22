ee80603429490d7211008ddd583557df8a643308
shipra.kumari1 <shipra.kumari1@olakrutrim.com>
2025-04-22 18:16:14 +0530

    server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT =  8080;
 
 // Health check endpoint
 app.get('/health_check', (req, res) => {
