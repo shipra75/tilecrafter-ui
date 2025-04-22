ee0dfb068a7d9720c955d2a854eee3c38bd65ecb
shipra.kumari1 <shipra.kumari1@olakrutrim.com>
2025-04-22 16:02:38 +0530

    fix server.js

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'src')));
 
-app.listen(PORT, () => {
-  console.log(`Frontend server is running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`Frontend server is running on http://0.0.0.0:${PORT}`);
 });
